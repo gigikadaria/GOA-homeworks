297c4564c748d889f415160597a6adc9522efd98
gigikadaria <dogecoinisgoingtothemoon@gmail.com>
2025-12-03 15:17:30 +0400

    second commit

---
+++ level18/homework/homework.py
@@ -0,0 +1,51 @@
+# 1
+for i in range(0, 39):
+    print(i)
+
+# 2
+for i in range(30, 41):
+    print(i)
+
+# 3
+for i in range(12, 121):
+    print(i)
+
+# 4
+name = "თქვენი სახელი"
+age = 20
+for i in range(18):
+    print(name, age)
+
+# 5
+for i in range(10, 101, 3):
+    print(i)
+
+# 6
+for i in range(1, 31):
+    print(i, "ფორთოხალი")
+
+# 7
+for i in range(13):
+    input("შეიყვანეთ თქვენი ასაკი: ")
+
+# 8
+name = "თქვენი სახელი"
+i = 0
+while i < 10:
+    print(name)
+    i += 1
+
+# 9
+n = 0
+while n <= 24:
+    print(n)
+    n += 1
+
+# 10
+for i in range(5):
+    print("for loop მაგალითი:", i)
+
+x = 0
+while x < 5:
+    print("while loop მაგალითი:", x)
+    x += 1
